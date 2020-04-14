f20f490083c4f544b8c693b115e82e302e176de5
midathanasiva <38171964+midathanasiva@users.noreply.github.com>
2020-04-14 20:28:52 +0530

    Add files via upload

---
+++ AssignmentApril09SpringBootFrameworkRestJpa_Midathana_siva/AssignmentApril09SpringBootFrameworkRestJpa/src/main/java/com/cg/AssignmentApril09SpringFrameworkRestJpa.java
@@ -0,0 +1,21 @@
+package com.cg;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class AssignmentApril09SpringFrameworkRestJpa {
+	
+
+
+	
+	
+		public static void main(String[] args) {
+			SpringApplication.run(AssignmentApril09SpringFrameworkRestJpa.class, args);
+		}
+
+	}
+
+	
+
+
